9660576c32e670c6fe54f253e16c8586211567ce
Takuto Komazaki <komazarari@gmail.com>
2020-04-14 01:07:54 +0000

    Add Lambda Layers

--- lib/index.ts
+++ lib/index.ts
@@ -10,11 +10,16 @@ export class CdkExampleLambdaWithLayer extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string, props: CdkExampleLambdaWithLayerProps = {}) {
     super(scope, id);
 
+    const layer = new lambda.LayerVersion(this, 'layer', {
+      code: lambda.Code.fromAsset(path.join(__dirname, '..', 'layer')),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_12_X],
+    });
     new lambda.Function(this, 'myHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,
       code: lambda.Code.fromAsset(path.join(__dirname, '..', 'lambda')),
       handler: 'index.handler',
       environment: { MY_MESSAGE: props.myMessage || 'Hello' },
+      layers: [layer],
     });
   }
 }
